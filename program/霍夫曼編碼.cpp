a43143933d9c5fad0d2774f847168b4da90308cb
oowoowe <130942510+oowoowe@users.noreply.github.com>
2025-03-05 17:55:51 +0800

    霍夫曼編碼.cpp

--- program/霍夫曼編碼.cpp
+++ program/霍夫曼編碼.cpp
@@ -16,32 +16,13 @@ public:
 		left = NULL; right = NULL;
 		ascii = a; weight = w;
 		str=n;
-	}
+	} 
 	mash() {
 		left = NULL; right = NULL;
-		ascii = NULL; weight = NULL;
+		ascii = -1; weight = -1;
 		str="";
 	}
-	void set(int W, int A, mash* L = NULL, mash* R = NULL) {
-		left = L; right = R;
-		ascii = A; weight = W;
-	}
-	void operator+(mash N) {
-		left = N.left; right = N.right;
-		weight = N.weight; ascii = N.ascii;
-	}
-	bool operator==(mash N) {
-		return left == N.left && right == N.right && weight == N.weight && ascii == N.ascii;
-	}
-	void operator=(const mash &N) {
-		left=N.left;
-		right=N.right;
-		weight=N.weight;
-		ascii=N.ascii;
-		str=N.str;
-	}
 	friend std::ostream& operator<<(std::ostream& out, const mash& m);
-
 	bool operator<(const mash& m)const {
 		if (weight == m.weight)return ascii < m.ascii;
 		else return weight < m.weight;
@@ -60,11 +41,11 @@ string serch(const mash &n,char target,string ans=""){
 
 
 int main() {
-	cout<<"type some words";
+	cout<<"type some words:\n";
 	int input[128]={};
 	string in; cin >> in;
-	for (char n : in) {
-		input[(int)n]++;
+	for (int count=0;in[count]!='\0';count++) {
+		input[(int)in[count]]++;
 	}
 	//
 	set<mash> S;
@@ -81,14 +62,13 @@ int main() {
 		a=new mash();b=new mash();
 		*a=*(S.begin());
 		*b=*(++S.begin());
-		auto it = S.begin();advance(it,2);
+		set<mash>::iterator it = S.begin();advance(it,2);
 		S.erase(S.begin(),it);
 		S.insert(mash(a,b));
 	}
 	//
-	cout << "Huffman Tree Root: " << *S.begin() << endl;
-	for (char n : in) {
-		cout<<serch(*S.begin(),n)<<" ";
+	for (int count=0;in[count]!='\0';count++) {
+		cout<<serch(*S.begin(),in[count])<<" ";
 	}
 
 }
