87385351cc31d868d80398433392c98e0e18ca25
oowoowe <130942510+oowoowe@users.noreply.github.com>
2025-03-13 20:45:24 +0800

    MUSH.cpp

--- program/MUSH.cpp
+++ program/MUSH.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+using namespace std;
 struct mush{
 	struct mush *left,*right;
 	int data;
 };
 struct mush* root;
-struct mush** serch(int n,struct mush* r){
-	if(!r||r->data==n)return &r;
-	else if(n>r->data)serch(n,r->right);
-	else serch(n,r->left);
+struct mush** serch(int n,struct mush** r){
+	if(!*r||(*r)->data==n)return r;
+	else if(n>(*r)->data)serch(n,&((*r)->right));
+	else serch(n,&((*r)->left));
 }
-bool add(int n){
-	struct mush **m=serch(n,root);
+bool add(int n,struct mush** r){
+	struct mush **m=serch(n,r);
 	if(*m)return 0;
 	else{
 		*m=(struct mush *)malloc(sizeof(struct mush));
@@ -20,6 +21,18 @@ bool add(int n){
 		return 1;
 	}
 }
+void show(struct mush* r){
+	if(r) {
+		if(r->left||r->right)cout<<r->data<<"[";
+		if(r->left)show(r->left);
+		if(r->left&&r->right)cout<<",";
+		if(r->right)show(r->right);
+		if(r->left||r->right)cout<<"]";
+	}
+	else return ;
+}
 int main(){
-	
+	root=NULL;
+	for(int i=0;i!=10;i++)add(i,&root);
+	show(root);
 }
