1b8fd795e213a799b74b00ecafafa7da2d1346fd
oowoowe <130942510+oowoowe@users.noreply.github.com>
2025-03-14 13:36:10 +0800

    Update MUSH.cpp

--- program/MUSH.cpp
+++ program/MUSH.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 using namespace std;
 struct mush{
 	struct mush *left,*right;
@@ -8,9 +9,28 @@ struct mush{
 struct mush* root;
 struct mush** serch(int n,struct mush** r){
 	if(!*r||(*r)->data==n)return r;
-	else if(n>(*r)->data)serch(n,&((*r)->right));
-	else serch(n,&((*r)->left));
+	if(n>(*r)->data)return serch(n,&((*r)->right));
+	return serch(n,&((*r)->left));
 }
+	string re="root"; 
+	void F(mush* r) {
+	    if (!r) return;
+	    re=re+"->"+to_string(r->data);
+		F(r->left);
+		F(r->right);
+	}
+	void M(mush* r) {
+	    if (!r) return;
+		M(r->left);
+		re=re+"->"+to_string(r->data);
+		M(r->right);
+	}
+	void B(mush* r) {
+	    if (!r) return;
+		B(r->left);
+		B(r->right);
+		re=re+"->"+to_string(r->data);
+	}
 bool add(int n,struct mush** r){
 	struct mush **m=serch(n,r);
 	if(*m)return 0;
@@ -21,18 +41,22 @@ bool add(int n,struct mush** r){
 		return 1;
 	}
 }
-void show(struct mush* r){
-	if(r) {
-		if(r->left||r->right)cout<<r->data<<"[";
-		if(r->left)show(r->left);
-		if(r->left&&r->right)cout<<",";
-		if(r->right)show(r->right);
-		if(r->left||r->right)cout<<"]";
-	}
-	else return ;
+string show(struct mush* r,void (*fun)(mush*)){
+	re="root";
+	fun(r);
+	return re;
+}
+bool is(mush* r,int n) {
+	if(!r)return 0;
+	if(r->data==n)return 1;
+	return (n>r->data) ? is(r->right,n) : is(r->left,n);
 }
 int main(){
+	srand(time(NULL));
 	root=NULL;
-	for(int i=0;i!=10;i++)add(i,&root);
-	show(root);
+	for(int i=0;i!=10;i++)add(rand() % 100, &root);
+	cout<<show(root,F)<<endl;
+	cout<<show(root,M)<<endl;
+	cout<<show(root,B)<<endl;
+
 }
